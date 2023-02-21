34b69b07cc3e8b264e1f8aef651bf85f30fef2cc
sami92news <sami.akram@92newshd.tv>
2023-02-21 14:03:34 +0500

    start elixir

---
+++ hello.ex
@@ -0,0 +1,20 @@
+st1 = "\nHello Elixir"
+lista = [3, "yyy", 5];
+#tuple1 = {4, "zzz", 5}
+list2 = [a: 1, b: 2]
+IO.inspect lista, label: "\nThe list is : "
+IO.puts("\nDict Item is #{inspect(list2[:a])}")
+cmp = 0
+if list2[:a] > cmp do
+  IO.puts "\nIs gt than #{cmp}"
+else
+  IO.puts "\nIs Not than #{cmp}"
+end
+lent = String.length(st1)
+ans = "\nLength is #{lent}"
+IO.puts(ans)
+
+IO.puts "\nMatch foo => #{String.match?("foo", ~r/foo/)}\n"
+
+
+#IO.puts tuple1
